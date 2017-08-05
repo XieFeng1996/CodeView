050ede8d3a96cff3b73453b1c3c4528f0355a333
海夕雨言 <3322649965@qq.com>
2017-08-05 23:16:16 +0800

    text
    doing text for more

---
+++ text/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  text
+//
+//  Created by 谢风 on 2017/8/5.
+//  Copyright © 2017年 Pyrrha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ text/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  text
+//
+//  Created by 谢风 on 2017/8/5.
+//  Copyright © 2017年 Pyrrha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
